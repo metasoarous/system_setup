1c132fb5f269f66565a65c052e4549af538a55a1
Christopher Small <metasoarous@gmail.com>
2014-04-27 12:52:48 -0700

    Seem to have solarize install working

--- bin/install_solarized.sh
+++ bin/install_solarized.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-echo $HOME on the range
-mkdir -p $HOME/encap
-cd $HOME/encap
+encap=/usr/local/encap
+mkdir -p $encap
+cd $encap
+
 git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 ./install.sh
-cd $HOME/encap
+
+cd $encap
 ln -s ../encap/gnome_terminal_solarized/set_dark.sh -t ../bin
 ln -s ../encap/gnome_terminal_solarized/set_light.sh -t ../bin
 ln -s ../encap/gnome_terminal_solarized/solarize -t ../bin
